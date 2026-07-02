7b40bf01ccfcc17ea7a54a610d9dceab0256c84a
Jiru Sun <42296811+yu-yake2002@users.noreply.github.com>
2026-07-02 14:32:20 +0800

    refactor(params): cute-fpe parameter integration (#3)
    - Move CuteFpeModule/CuteFpeBundle ownership into cute-fpe and use CDE parameters directly, so CUTE can reuse cute-fpe configuration classes without duplicate definitions.

--- fpe/src/main/scala/top/CuteFpeParameters.scala
+++ fpe/src/main/scala/top/CuteFpeParameters.scala
@@ -1,5 +1,8 @@
 package cute
 
+import chisel3._
+import org.chipsalliance.cde.config._
+
 trait CuteFpeParameters {
   def cuteFpeConfig: CuteFpeConfig
 
@@ -20,3 +23,52 @@ trait CuteFpeParameters {
   def DEBUG_FP4: Boolean
 }
 
+object DefaultCuteFpeParameters extends CuteFpeParameters {
+  def cuteFpeConfig: CuteFpeConfig = CuteFpeConfig()
+
+  def ReduceWidth: Int = 512
+  def ResultWidth: Int = 32
+  def MinGroupSize: Int = 16
+  def MinDataTypeWidth: Int = 4
+  def ScaleElementWidth: Int = 8
+  def cmptreelayers: Int = 4
+  def fp8cmptreelayers: Int = 4
+
+  def P3AddNum: Int = ReduceWidth / 4 / MinGroupSize
+  def P2AddNum: Int = ReduceWidth / (P3AddNum * 16)
+
+  def FP4P0AddNum: Int = 2
+  def FP4P1AddNum: Int = 16 / FP4P0AddNum
+
+  def DEBUG_FP8: Boolean = true
+  def DEBUG_FP4: Boolean = false
+}
+
+case object CuteFpeParamsKey extends Field[CuteFpeParameters](DefaultCuteFpeParameters)
+
+trait HasCuteFpeParameters extends CuteFpeParameters {
+  implicit val p: Parameters
+
+  private def fpeParams: CuteFpeParameters = p(CuteFpeParamsKey)
+
+  def cuteFpeConfig: CuteFpeConfig = fpeParams.cuteFpeConfig
+
+  def ReduceWidth: Int = fpeParams.ReduceWidth
+  def ResultWidth: Int = fpeParams.ResultWidth
+  def MinGroupSize: Int = fpeParams.MinGroupSize
+  def MinDataTypeWidth: Int = fpeParams.MinDataTypeWidth
+  def ScaleElementWidth: Int = fpeParams.ScaleElementWidth
+  def cmptreelayers: Int = fpeParams.cmptreelayers
+  def fp8cmptreelayers: Int = fpeParams.fp8cmptreelayers
+
+  def P3AddNum: Int = fpeParams.P3AddNum
+  def P2AddNum: Int = fpeParams.P2AddNum
+  def FP4P0AddNum: Int = fpeParams.FP4P0AddNum
+  def FP4P1AddNum: Int = fpeParams.FP4P1AddNum
+
+  def DEBUG_FP8: Boolean = fpeParams.DEBUG_FP8
+  def DEBUG_FP4: Boolean = fpeParams.DEBUG_FP4
+}
+
+class CuteFpeModule(implicit val p: Parameters) extends Module with HasCuteFpeParameters
+class CuteFpeBundle(implicit val p: Parameters) extends Bundle with HasCuteFpeParameters
